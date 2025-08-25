05febe14b590602620892ea761fc53a95f3e5252
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2025-08-25 14:16:33 -0300

    Update vendaController.js

--- src/controllers/vendaController.js
+++ src/controllers/vendaController.js
@@ -1,55 +1,69 @@
-const Venda = require('../models/venda');
-const Produto = require('../models/produto');
+const Venda = require('../models/venda')
+const Produto = require('../models/produto')
 
 const criarVenda = async (req, res) => {
   try {
-    const { produtos } = req.body;
+    const { produtos } = req.body
 
-    let total = 0;
-    const detalhesProdutos = [];
+    let total = 0
+    const detalhesProdutos = []
+    const produtosAtualizados = []
 
     for (let item of produtos) {
-      const produto = await Produto.findOne({ nome: item.nomeProduto });
+      const produto = await Produto.findOne({ nome: item.nomeProduto })
 
       if (!produto || produto.quantidade < item.quantidade) {
-        return res.status(400).json({ erro: `Produto "${item.nomeProduto}" não encontrado ou estoque insuficiente` });
+        for (let p of produtosAtualizados) {
+          const original = await Produto.findById(p._id)
+          original.quantidade += p.quantidadeAlterada
+          await original.save()
+        }
+
+        return res.status(400).json({
+          erro: `Produto "${item.nomeProduto}" não encontrado ou estoque insuficiente`
+        })
       }
 
-      const subtotal = item.valor * item.quantidade;
-      total += subtotal;
+      const subtotal = item.valor * item.quantidade
+      total += subtotal
 
       detalhesProdutos.push({
         nomeProduto: item.nomeProduto,
         quantidade: item.quantidade,
         valor: item.valor,
         subtotal
-      });
+      })
 
       produto.quantidade -= item.quantidade;
-      await produto.save();
+      await produto.save()
+
+      produtosAtualizados.push({
+        _id: produto._id,
+        quantidadeAlterada: item.quantidade
+      })
     }
 
     const novaVenda = new Venda({
       produtos: detalhesProdutos,
       total
-    });
+    })
 
-    await novaVenda.save();
+    await novaVenda.save()
 
-    res.status(201).json(novaVenda);
+    res.status(201).json(novaVenda)
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ erro: 'Erro ao criar venda' });
+    console.error(err)
+    res.status(500).json({ erro: 'Erro ao criar venda' })
   }
 };
 
 const listarVendas = async (req, res) => {
   try {
-    const vendas = await Venda.find();
-    res.json(vendas);
+    const vendas = await Venda.find()
+    res.json(vendas)
   } catch (err) {
-    res.status(500).json({ erro: 'Erro ao listar as vendas' });
+    res.status(500).json({ erro: 'Erro ao listar as vendas' })
   }
-};
+}
 
-module.exports = { criarVenda, listarVendas };
+module.exports = { criarVenda, listarVendas }
