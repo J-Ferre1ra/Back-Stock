c5d072e5902aa5e68adff63627d753ca1f65977d
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2025-11-29 00:18:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const app = require('./src/app')
 const PORT = process.env.PORT || 3000
+const cors = require('cors');
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://estoquemaster.vercel.app"
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
 app.listen(PORT, () =>{
     console.log(`Servidor rodando na porta ${PORT}`);
